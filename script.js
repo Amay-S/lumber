c673048c0c348f56be8f0a3e137fcd4b26fbdb7e
Amay <amay.shah10@gmail.com>
2023-11-02 10:45:45 -0400

    remove node js backend

--- script.js
+++ script.js
@@ -13,36 +13,36 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// contact us 
+// // contact us 
 
-document.querySelector('form').addEventListener('submit', async function (e) {
-    e.preventDefault();
+// document.querySelector('form').addEventListener('submit', async function (e) {
+//     e.preventDefault();
 
-    const data = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value
-    };
+//     const data = {
+//         name: document.getElementById('name').value,
+//         email: document.getElementById('email').value,
+//         subject: document.getElementById('subject').value,
+//         message: document.getElementById('message').value
+//     };
 
-    // Check if any field is empty
-    if (!data.name || !data.email || !data.subject || !data.message) {
-        alert('Please fill in all fields');
-        return;
-    }
+//     // Check if any field is empty
+//     if (!data.name || !data.email || !data.subject || !data.message) {
+//         alert('Please fill in all fields');
+//         return;
+//     }
 
-    try {
-        const response = await fetch('http://localhost:3000/sendmail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+//     try {
+//         const response = await fetch('http://localhost:3000/sendmail', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         });
 
-        const responseData = await response.json();
-        alert(responseData.message);
-    } catch (error) {
-        alert('Error sending email');
-    }
-});
+//         const responseData = await response.json();
+//         alert(responseData.message);
+//     } catch (error) {
+//         alert('Error sending email');
+//     }
+// });

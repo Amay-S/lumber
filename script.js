27688e05c852340aeaf45517fbc1bffcc9b391f1
Amay <amay.shah10@gmail.com>
2023-11-03 00:27:22 -0400

    more videos added and fixed product pics

--- script.js
+++ script.js
@@ -13,6 +13,37 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// video section 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let index = 0;
+    const videoWrappers = document.querySelectorAll('.carousel-container .video-wrapper');
+    const indicators = document.querySelectorAll('.carousel-indicators .indicator');
+    const prevButton = document.querySelector('.carousel-btn-prev');
+    const nextButton = document.querySelector('.carousel-btn-next');
+    
+    function moveCarousel(direction = 'next') {
+        videoWrappers[index].classList.remove('active');
+        indicators[index].classList.remove('active');
+
+        if (direction === 'next') {
+            index = (index + 1) % videoWrappers.length; // Move to the next video
+        } else if (direction === 'prev') {
+            index = (index - 1 + videoWrappers.length) % videoWrappers.length; // Move to the previous video
+        }
+
+        videoWrappers[index].classList.add('active');
+        indicators[index].classList.add('active');
+    }
+    
+    setInterval(() => moveCarousel('next'), 10000); // Move carousel every 5 seconds
+
+    prevButton.addEventListener('click', () => moveCarousel('prev'));
+    nextButton.addEventListener('click', () => moveCarousel('next'));
+});
+
+
+
 // // contact us 
 
 // document.querySelector('form').addEventListener('submit', async function (e) {
